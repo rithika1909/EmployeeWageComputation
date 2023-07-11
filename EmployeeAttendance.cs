9130b90cb6a0a83598659cee399f46d074b1c5e7
Rithika L <rithika.l@tvsnext.com>
2023-07-11 14:53:19 +0530

    [Rithika] Add : Implemented Daily Emp Wage

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
 namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8;
         public void EmployeeAttendance()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == 0)
-            {
+            
                 Console.WriteLine("Employee is present");
           
             else
                 Console.WriteLine("Employee is Absent");
         }
+        public void CalculateEmpWage()
+        {
+                int dailyEmpWage = WAGE_PER_HR + FULL_DAY_HR;
+                Console.WriteLine(dailyEmpWage);
+
+        }
+
     }
 }
