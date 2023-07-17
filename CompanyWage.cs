aa5f512c30cdeb03421e1e99c3ea4113b5b488ad
Rithika L <rithika.l@tvsnext.com>
2023-07-17 16:32:48 +0530

    [Rithika] Add : Implemented

--- CompanyWage.cs
+++ CompanyWage.cs
@@ -8,11 +8,47 @@ namespace EmployeeWageComputation
 {
     public class CompanyWage
     {
-        const int FULL_DAY_HR = 8, PART_DAY_HR = 4, IS_FULL_TIME = 0, IS_PART_TIME = 1;
+        const int FULL_DAY_HR = 8, PART_DAY_HR = 4,
+            IS_FULL_TIME = 0, IS_PART_TIME = 1;
         int totalEmpWage = 0, empHrs = 0, wagePerHr, totalWorkingDays, totalWorkingHrs;
         string companyName;
-        Random random = new Random();
-        public Employee(string companyName, int wagePerHour, int totalWorkingDays, int totalWorkingHrs);
+        Random Random = new Random();
+        public CompanyWage(string companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs)
+        {
+            this.companyName = companyName;
+            this.wagePerHr = wagePerHr;
+            this.totalWorkingDays = totalWorkingDays;
+            this.totalWorkingHrs = totalWorkingHrs;
+        }
+        public void EmployeeAttendance()
+        {
+            int empCheck = Random.Next(0, 2);//returns value 0, 1
+            if (empCheck == 0)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
 
+        }
+        public void CalculateEmpWage()
+        {
+            for (int i = 0; i < totalWorkingDays && empHrs < totalWorkingHrs; i++)
+            {
+                int empCheck = Random.Next(0, 3);//0,1,2
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = FULL_DAY_HR;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = PART_DAY_HR;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+            }
+            totalEmpWage = wagePerHr * empHrs;
+            Console.WriteLine(companyName + "--->" + totalEmpWage);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ namespace EmployeeWageComputation
         static void Main(String[] args)
         {
             Console.WriteLine("Welcome to EmployeeWageComputation");
-            EmployeeWage wage = new EmployeeWage();
-            wage.CalculateEmpWage();
+            CompanyWage appleWage = new CompanyWage("Apple", 200, 20, 100);
+            //employeeWage.EmployeeAttendance();
+            appleWage.EmployeeAttendance();
+            appleWage.CalculateEmpWage();
+            CompanyWage amazonWage = new CompanyWage("Amazon", 100, 20, 100);
+            amazonWage.EmployeeAttendance();
+            amazonWage.CalculateEmpWage();
         }
     }
 }
